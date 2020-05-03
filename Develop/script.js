df047553150d20496e1281a146e487191b79c662
DAYMAN <andrew.morrow5510@gmail.com>
2020-05-03 10:59:37 -0700

    got javascript working and completed homework

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-(function() {
+$(document).ready(function() {
   
     // test flag
     var test = false;
@@ -21,7 +21,7 @@
     
     // using font awesome icon https://fontawesome.com/license
     // change description here - none
-    var saveIcon = "assets/floppy-icon.png"; 
+    var saveIcon = "./assets/floppy-icon.png"; 
   
     // Get stored todos from localStorage
     // Parsing the JSON string to an object
@@ -50,28 +50,28 @@
   
   
     // build calendar by row for fix set of hours
-    for (let hour = 9; hour <= 17; hour++) {
+    for (var hour = 9; hour <= 17; hour++) {
       // index for array use offset from hour
-      let index = hour - 9;
+      var index = hour - 9;
       
       // build row components
-      let $rowDiv = $('<div>');
+      var $rowDiv = $('<div>');
       $rowDiv.addClass('row');
       $rowDiv.addClass('plannerRow');
       $rowDiv.attr('hour-index',hour);
     
       // Start building Time box portion of row
-      let $col2TimeDiv = $('<div>');
+      var $col2TimeDiv = $('<div>');
       $col2TimeDiv.addClass('col-md-2');
     
       // create timeBox element (contains time)
-      const $timeBoxSpn = $('<span>');
+      var $timeBoxSpn = $('<span>');
       // can use this to get value
       $timeBoxSpn.attr('class','timeBox');
       
       // format hours for display
-      let displayHour = 0;
-      let ampm = "";
+      var displayHour = 0;
+      var ampm = "";
       if (hour > 12) { 
         displayHour = hour - 12;
         ampm = "pm";
@@ -90,7 +90,7 @@
   
       // START building input portion of row
       // build row components
-      let $dailyPlanSpn = $('<input>');
+      var $dailyPlanSpn = $('<input>');
   
       $dailyPlanSpn.attr('id',`input-${index}`);
       $dailyPlanSpn.attr('hour-index',index);
@@ -101,7 +101,7 @@
       $dailyPlanSpn.val( planTextArr[index] );
       
       // create col to control width
-      let $col9IptDiv = $('<div>');
+      var $col9IptDiv = $('<div>');
       $col9IptDiv.addClass('col-md-9');
   
       // add col width and row component to row
@@ -110,10 +110,10 @@
       // STOP building Time box portion of row
   
       // START building save portion of row
-      let $col1SaveDiv = $('<div>');
+      var $col1SaveDiv = $('<div>');
       $col1SaveDiv.addClass('col-md-1');
   
-      let $saveBtn = $('<i>');
+      var $saveBtn = $('<i>');
       $saveBtn.attr('id',`saveid-${index}`);
       $saveBtn.attr('save-id',index);
       $saveBtn.attr('class',"far fa-save saveIcon");
@@ -155,10 +155,10 @@
   
       if (test) { console.log('click pta before '+ planTextArr); }
   
-      let $index = $(this).attr('save-id');
+      var $index = $(this).attr('save-id');
   
-      let inputId = '#input-'+$index;
-      let $value = $(inputId).val();
+      var inputId = '#input-'+$index;
+      var $value = $(inputId).val();
   
       planTextArr[$index] = $value;
   
@@ -180,7 +180,7 @@
   
       // neeed to check for save button
   
-      let i = $(this).attr('hour-index');
+      var i = $(this).attr('hour-index');
   
       // add shawdow pulse class
       $(`#saveid-${i}`).addClass('shadowPulse');
